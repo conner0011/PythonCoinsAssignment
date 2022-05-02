ce3dded0c48ab0f010d656ebf358e7811120e8e5
conner <connerdgreen@gmail.com>
2022-05-02 17:57:55 -0500

    Initial commit

---
+++ CoinsAssignment.py
@@ -0,0 +1,15 @@
+﻿def main():
+    owed = float(input("How much change is owed? "))
+    quarters = owed // .25
+    dimes = (owed % .25) // .10
+    nickels = ((owed % .25) % .10) // .05
+    pennies = (((owed % .25) % .10) % .05) // .01
+    if quarters > 0:
+        print("You need", quarters, "quarters.")
+    if dimes > 0:
+        print("You need", dimes, "dimes.")
+    if nickels > 0:
+        print("You need", nickels, "nickels.")
+    if pennies > 0:
+        print("You need", pennies + 1, "pennies.")
+main()
